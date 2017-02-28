19759d17e2bea7fb3c252a86add63c84a7f3e082
DepKost <depakismile@gmail.com>
2017-02-28 23:50:53 +0200

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+import random as rd
+
+random_input = rd.sample(range(0, 10), 10)
+print random_input
+
+
+maxindx = np.argmax(random_input)
+#print max_indx
+
+new = np.delete(random_input, maxindx)
+minindx = np.argmin(new)
+new = np.delete(new, minindx)
+print new
+
+new = np.delete(random_input, maxindx)
+minindx = np.argmin(new)
+new = np.delete(new, minindx)
+print new
+
+def function_std(array):
+maxval = max(random_input)
+    #print max_val
+maxindx = np.argmax(random_input)
+    #print max_indx
+minval = min(random_input)
+new = np.delete(random_input, maxindx)
+minindx = np.argmin(new)
+new = np.delete(new, minindx)
+result = np.std(new)
+    #print result
+ return result
+	
+random_input = rd.sample(range(0, 10), 10)
+print random_input
+
+function_std(random_input)
